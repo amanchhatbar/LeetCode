78ffad5edc2497559ab6035e13b43591739c64b6
amanchhatbar <aman231091@gmail.com>
2021-10-09 16:48:26 -0500

    * HowSum.cs: * Program.cs: * Leetcode.dll: * Leetcode.pdb: * Leetcode.dll: * Leetcode.pdb:
    * UserPrefs.xml: Merge new code for HowSum

--- Leetcode/HowSum.cs
+++ Leetcode/HowSum.cs
@@ -1,9 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Leetcode
 {
     public class HowSum
     {
 
 
+        public List<int> HowSumRecursive(int target, int[] numbers, List<int> resultList)
+        {
+            if (resultList == null)
+            {
+                resultList = new List<int>();
+            }
+            if (target < 0) return null;
+            if (target == 0) return new List<int>() ;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var result = HowSumRecursive(target - numbers[i], numbers, resultList);
+                if (result != null)
+                {
+                    resultList.Add(target);
+                    Console.WriteLine(target);
+                }
+            }
+
+            return null;
+        }
+
     }
+
+
 }
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -7,9 +7,11 @@ namespace Leetcode
         static void Main(string[] args)
         {
 
-            CanSum cs = new CanSum();
-            Console.WriteLine(cs.CanSumMethod(7, new int[] { 5, 3, 4, 7 }));
-            Console.WriteLine(cs.CanSumMethod(300, new int[] { 7, 14 }));
+            HowSum cs = new HowSum();
+            List<int> results = new List<int>();
+            var e = cs.HowSumRecursive(7, new int[] { 5, 3, 4, 7 }, results);
+            Console.WriteLine(cs.HowSumRecursive(7, new int[] { 5, 3, 4, 7 }, results));
+            Console.WriteLine(cs.HowSumRecursive(300, new int[] { 7, 14 }, results));
 
             //GridTraveller g = new GridTraveller();
             //Console.WriteLine(g.TravelGridRecursivelyMemoization(18, 18));
