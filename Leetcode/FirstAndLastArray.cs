c14da9c31406e95439ed2c675af041e39c7c7f6b
amanchhatbar <aman231091@gmail.com>
2021-10-10 16:22:10 -0500

    add

--- Leetcode/FirstAndLastArray.cs
+++ Leetcode/FirstAndLastArray.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 namespace Leetcode
 {
-    public class FirstAndLastArray
+    public class FirstAndLastArrays
     {
-        public int[] SearchRange(int[] nums, int target)
+       
+        public int[] Recursive(int start, int end, int[] nums, int target)
         {
+            var mid = (start + end) / 2;
+            if(nums[mid] == target)
+            {
+                var secondValue = 0;
+                if (target == nums[mid + 1])
+                {
+                    secondValue = mid + 1;
+                }
+                else
+                {
+                    secondValue = mid - 1;
+                }
+
+                var temp = mid < secondValue ? new int[] { mid, secondValue } : new int[] { secondValue, mid };
+
+                return temp;
+            }
+
+            if(start == end)
+            {
+                return new int[] { };
+            }
+
+            if(target < nums[mid])
+            {
+                return Recursive(start, mid - 1, nums, target);
+            }
+            else
+            {
+                return Recursive(mid + 1, end, nums, target);
+            }
+
 
         }
 
-
-        public void nonRecursion(int[] nums, int target)
+        public int[] nonRecursion(int[] nums, int target)
         {
             int start = 0, end = nums.Length - 1, mid = 0;
             //var mid = (start + end) / 2;
@@ -19,20 +51,34 @@ namespace Leetcode
                 mid = (start + end) / 2;
                 if (target == nums[mid])
                 {
-                    //TODO: Check the neighbouring elements 
+                    var secondValue = 0;
+                    if (target == nums[mid + 1])
+                    {
+                        secondValue = mid + 1;
+                    }
+                    else
+                    {
+                        secondValue = mid - 1;
+                    }
+
+                    var temp = mid < secondValue ? new int[]{ mid, secondValue } : new int[]{ secondValue, mid };
+
+                    return temp;
                 }
 
                 if(target <= nums[mid])
                 {
-                    start = 0;
+                    //start = 0;
                     end = mid - 1;
                 }
                 else
                 {
                     start = mid + 1;
-                    end = end;
+                    //end = end;
                 }
             }
+
+            return new int[] { -1, -1 };
         }
     }
 }
