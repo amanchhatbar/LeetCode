d5eaa7f53c28a00b0c9af12755676a7598dfb7bc
amanchhatbar <32755035+amanchhatbar@users.noreply.github.com>
2021-10-21 09:08:28 -0500

    update

---
+++ Leetcode/ZigZagConversion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Leetcode
+{
+    public class ZigZagConversion
+    {
+        public ZigZagConversion()
+        {
+        }
+
+        public string Convert(string s, int numRows)
+        {
+
+            var stringChar = s.ToCharArray();
+            int j = 0;
+            for (int i = 0; i < 100; i++) //Column TODO: MaxLimit
+            {
+                for (; j < numRows; j++) //Row
+                {
+                    if(i / (numRows - 1) == 0)
+                    {
+                        j = 1;
+                        break;
+                       
+                    }
+                }
+                j--;
+            }
+
+
+
+            return s;
+        }
+    }
+}
+
