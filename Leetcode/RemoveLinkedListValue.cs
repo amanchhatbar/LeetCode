374ca34f87d012da876d5a95b8dc2ccd8c70567a
amanchhatbar <32755035+amanchhatbar@users.noreply.github.com>
2021-11-11 19:18:21 -0600

    LinkedList

---
+++ Leetcode/RemoveLinkedListValue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Leetcode
+{
+    public class RemoveLinkedListValue
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            if(head == null)
+            {
+                return head;
+            }
+
+
+
+            ListNode prev = new ListNode();
+            prev.next = head;
+            ListNode current = prev;
+            while (current.next != null)
+            {
+                if (current.next.val == val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+
+                    current = current.next;
+                }
+
+            }
+
+            return prev.next;
+        }
+
+
+
+    }
+}
+
