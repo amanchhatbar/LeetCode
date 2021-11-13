314d61dc9bfb3f3440a5cf9ef8d5bca54fa495fd
amanchhatbar <32755035+amanchhatbar@users.noreply.github.com>
2021-11-13 10:48:42 -0600

    update

---
+++ Leetcode/DailyTemps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Leetcode
+{
+    public class DailyTemps
+    {
+        System.Collections.Stack stack = new System.Collections.Stack();
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            int[] result = new int[temperatures.Length - 1];
+
+            var totalLength = temperatures.Length - 1;
+            for (int i = 0; i < totalLength; i++)
+            {
+                while (totalLength > 0 && temperatures[1] > temperatures[(int)stack.Peek()])
+                {
+                    result[(int)stack.Peek()] = i - (int)stack.Peek();
+                    stack.Pop();
+                }
+                stack.Push(i);
+
+            }
+            return result;
+        }
+    }
+}
+
